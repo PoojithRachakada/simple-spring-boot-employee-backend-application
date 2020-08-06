b8555438750d5d8d00042c42cbe1a4d96614a206
PoojithRachakada <48828785+PoojithRachakada@users.noreply.github.com>
2020-08-07 00:07:08 +0530

    Add files via upload

---
+++ springboot-backend/src/main/java/net/javaguides/springbootbackend/controller/ReponseEntity.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootbackend.controller;
+
+import org.springframework.http.ResponseEntity;
+
+import net.javaguides.springbootbackend.model.Employee;
+
+public class ReponseEntity<T> {
+
+	public static ResponseEntity<Employee> ok(Employee employee) {
+		return null;
+	}
+
+}
